43cec17622d439b28db023ada14b37cd5797cdca
Stephen Lemp <sl@stephenlemp.com>
2025-10-24 11:00:03 -0400

    list reoprts if no report id in portlet

--- SL Report Runner/src/FileCabinet/SuiteScripts/sl/reportRunner/portlet.js
+++ SL Report Runner/src/FileCabinet/SuiteScripts/sl/reportRunner/portlet.js
@@ -9,16 +9,13 @@ define(['N/runtime', 'N/file', 'N/ui/serverWidget', 'N/search'], function (runti
 
   function renderContent(params) {
     const reportId = runtime.getCurrentScript().getParameter({ name: 'custscript_slrr_portlet_reportid' });
-    if (!reportId) {
-      params.portlet.html = [
-        '<div class="slrr-portlet-message" style="font-size:12px;line-height:1.4;">',
-        '<p style="margin:0;">No report selected. Edit the portlet preferences to choose a report.</p>',
-        '</div>'
-      ].join('');
-      return;
-    }
+    if (!reportId) { renderReportList(params.portlet); }
+    else { renderReportOutput(portlet, reportId); }
+  }
 
-    params.portlet.title = search.lookupFields({
+
+  function renderReportOutput(portlet, reportId) {
+    portlet.title = search.lookupFields({
       type: 'customrecord_sl_reportrunnerconfig',
       id: reportId,
       columns: ['name']
@@ -28,13 +25,13 @@ define(['N/runtime', 'N/file', 'N/ui/serverWidget', 'N/search'], function (runti
     const script = `<script>${file.load({ id: '/SuiteScripts/sl/reportRunner/runReport/index.js' }).getContents()}</script>`;
     const body = `<body>${file.load({ id: '/SuiteScripts/sl/reportRunner/runReport/index.html' }).getContents()}</body>`;
 
-    params.portlet.addField({
+    portlet.addField({
       id: 'custpage_reportoutput',
       type: serverWidget.FieldType.INLINEHTML,
       label: 'Report Output'
     }).defaultValue = `${style}${script}${body}`;
 
-    params.portlet.addField({
+    portlet.addField({
       id: 'custpage_reportid',
       type: serverWidget.FieldType.TEXT,
       label: 'Report ID'
@@ -42,6 +39,21 @@ define(['N/runtime', 'N/file', 'N/ui/serverWidget', 'N/search'], function (runti
       .updateDisplayType({ displayType: serverWidget.FieldDisplayType.HIDDEN })
       .defaultValue = reportId;
   }
+
+  function renderReportList(portlet) {
+    portlet.title = 'Reports List';
+
+    const style = `<style>${file.load({ id: '/SuiteScripts/sl/reportRunner/listReports/index.css' }).getContents()}</style>`;
+    const script = `<script>${file.load({ id: '/SuiteScripts/sl/reportRunner/listReports/index.js' }).getContents()}</script>`;
+    const body = `<body>${file.load({ id: '/SuiteScripts/sl/reportRunner/listReports/index.html' }).getContents()}</body>`;
+
+    portlet.addField({
+      id: 'custpage_reportslisting',
+      type: serverWidget.FieldType.INLINEHTML,
+      label: 'Report Listing'
+    }).defaultValue = `${style}${script}${body}`;
+  }
+
 
   return {
     render: renderContent
